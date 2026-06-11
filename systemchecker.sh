a28a0d4e4f2b24c7e40ae595007ef4cc8e35349f
Zain Shahid <mzainshahid22@gmail.com>
2026-06-11 15:58:25 +0500

    Update systemchecker.sh

--- systemchecker.sh
+++ systemchecker.sh
@@ -5,13 +5,15 @@ echo "             SYSTEM HEALTH CHECKER                   "
 echo "====================================================="
 
 
-
+# TO CHECK THE DISK USAGE BY FILES
 echo " === DISK USAGE CHECK ==="
+
 uptime
+
 df -h | grep -E "/dev/"
 
 echo ""
-
+# TO CHECK THE MEMORY USAGE USED , FREE etc 
 echo " === MEMORY USAGE CHECK == "
 
 free -h
